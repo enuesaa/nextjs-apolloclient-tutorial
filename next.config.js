5c6ab3a241dd605591596d66451727707e2ea30f
nsrciog <nsrciog@gmail.com>
2022-08-06 11:29:23 +0900

    feat: use sass

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  sassOptions: {
+    includePaths: ['./styles'],
+    prependData: `@import "./variables.scss";`
+  }
 }
 
 module.exports = nextConfig

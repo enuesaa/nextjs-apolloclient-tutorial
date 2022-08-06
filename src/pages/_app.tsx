98f11ac90a812d1fc60806e298f2e83b8a3db4de
nsrciog <nsrciog@gmail.com>
2022-08-06 13:23:03 +0900

    feat: add client.ts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,10 @@
 import type { AppProps } from 'next/app'
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
+import { ApolloProvider } from '@apollo/client'
+import { client } from '@/graph/client'
 import '@/styles/globals.scss'
 import 'modern-css-reset/dist/reset.min.css'
 
 export default function App({ Component, pageProps }: AppProps) {
-  const client = new ApolloClient({
-    uri: 'https://flyby-gateway.herokuapp.com/',
-    cache: new InMemoryCache(),
-  })
-
   return (
     <ApolloProvider client={client}>
       <Component {...pageProps} />

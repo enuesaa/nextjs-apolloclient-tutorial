8d8ded020482f65e57f4cfeffdfd6ffca8f2fdb0
nsrciog <nsrciog@gmail.com>
2022-08-06 11:41:44 +0900

    feat: `Step 3: Initialize ApolloClient`
    https://www.apollographql.com/docs/react/get-started#step-3-initialize-apolloclient

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,27 @@
 import type { NextPage } from 'next'
+import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client'
 
 const Home: NextPage = () => {
+  const client = new ApolloClient({
+    uri: 'https://flyby-gateway.herokuapp.com/',
+    cache: new InMemoryCache(),
+  });
+
+  client
+    .query({
+      query: gql`
+        query GetLocations {
+          locations {
+            id
+            name
+            description
+            photo
+          }
+        }
+      `,
+    })
+    .then((result) => console.log(result));
+
   return <div></div>
 }
 
